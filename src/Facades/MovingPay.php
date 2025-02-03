72975c45f825bc9b7dd9567f35e52cb7933b65de
Victor Danilo <victordanilo_cs@live.com>
2025-02-03 16:24:59 -0300

    feat: add Laravel integration with Facades and Service Provider

---
+++ src/Facades/MovingPay.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the MovingPay SDK.
+ *
+ * (c) Victor Danilo <victordanilo_cs@live.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Husail\MovingPay\Facades;
+
+use Husail\MovingPay\Client;
+use Illuminate\Support\Facades\Facade;
+
+/**
+ * @see Client
+ */
+class MovingPay extends Facade
+{
+    protected static function getFacadeAccessor(): string
+    {
+        return Client::class;
+    }
+
+    public static function client()
+    {
+        return self::getFacadeRoot();
+    }
+}
